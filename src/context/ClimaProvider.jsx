56d7334f899c6123478ba78a8e431ccaaddc7589
juanvacag <>
2022-06-26 21:44:22 +0200

    final

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -12,9 +12,9 @@ const ClimaProvider = ({children}) => {
         pais: ''
     })
 
-    const [resultado, setResultado] = useState({
-
-    })
+    const [resultado, setResultado] = useState({})
+    const [cargando, setCargando] = useState(false)
+    const [noResultado, setNoResultado] = useState(false)
 
     const datosBusqueda = e => {
         setBusqueda({
@@ -24,27 +24,29 @@ const ClimaProvider = ({children}) => {
     }
 
     const consultarClima = async datos => {
-        //console.log(datos)
+        setCargando(true)
+        setNoResultado(false)
+        
         try {
             const {ciudad, pais} = datos
 
             const appId = import.meta.env.VITE_API_KEY
 
             const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
-            //console.log(url)
 
             const {data} = await axios(url)
-            //console.log(data[0])
             const {lat, lon} =data[0]
 
             const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
 
             const {data: clima} = await axios(urlClima)
-            //console.log(clima)
             setResultado(clima)
 
         } catch (error) {
-            console.log(error)
+            //console.log(error)
+            setNoResultado('No hay resultados')
+        }finally {
+            setCargando(false)
         }
     }
 
@@ -54,7 +56,9 @@ const ClimaProvider = ({children}) => {
                 busqueda,
                 datosBusqueda,
                 consultarClima,
-                resultado
+                resultado,
+                cargando,
+                noResultado
             }}
     
     >
